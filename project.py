81ed30dd1703e9d515774ac321b50d3d04e7a669
vagrant <vagrant@vagrant.vm>
2018-09-14 23:01:02 +0000

    Added DB functionality

--- project.py
+++ project.py
@@ -12,17 +12,40 @@ def db_connection():
 
 # Function  to answer question: "What are the most popular three articles of all time?"
 def three_articles():
-	db = db_connection()
+		# Data source
+		db = db_connection()
+		cur = db.cursor()
+		query = "SELECT articles.author, authors.name FROM articles, authors WHERE articles.author = authors.id;"
+		
+		# Get the data
+		cur.execute(query)
+		rows = cur.fetchall()
+
+		# Show me the raw data
+		print("Row Data: ")
+		print(rows)
+
+		# Parse the data
+		print
+		print "Author Names:"
+		for row in rows:
+			print " ", row[1]
+
+		# Close connections
+		cur.close()
+		db.close()
 
 # Function to answer question: "Who are the most popular articles authors of all time?"
 def three_authors():
 	db = db_connection()
-
+	cur = db.cursor()
 
 # Function to answer question: "On which days did more than 1% of requests lead to errors"
 def request_errors():
 	db = db_connection()
+	cur = db.cursor()
 
+three_articles()
 # define query
 # cursor object
 # execute query with cursor object, place into rows variable?
