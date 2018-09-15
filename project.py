06674acdd52332b42422969b35c5d39155a66c62
elronF <lflearns@gmail.com>
2018-09-15 21:20:07 +0000

    Added second query

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
 import psycopg2
 
-
 # Function to create a connection to the DB, since we'll need to do it three times.
 def db_connection():
 	dbName = "dbname=news"
@@ -12,35 +11,52 @@ def db_connection():
 
 # Function  to answer question: "What are the most popular three articles of all time?"
 def three_articles():
-		# Data source
 		db = db_connection()
 		cur = db.cursor()
+		
 		query = '''SELECT a.title, count(b.path) AS num 
 				   FROM articles a 
 				   JOIN log b 
-						ON b.path LIKE CONCAT ('%', a.slug, '%') 
+						ON b.path LIKE CONCAT ('%', a.slug) 
 				   WHERE b.path <> '/' 
 				   GROUP BY a.title 
 				   ORDER BY num DESC LIMIT 8;'''
 		
-		# Get the data
 		cur.execute(query)
 		rows = cur.fetchall()
 
-		# Parse the data
 		print
 		print "Most popular articles:"
 		for row in rows:
 			print " ", row[0], "-", row[1], "views"
 
-		# Close connections
 		cur.close()
 		db.close()
 
 # Function to answer question: "Who are the most popular articles authors of all time?"
 def three_authors():
-	db = db_connection()
-	cur = db.cursor()
+		db = db_connection()
+		cur = db.cursor()
+		
+		query = '''SELECT authors.name, count(log.path) AS views 
+		FROM articles 
+		JOIN authors 
+			ON articles.author = authors.id 
+		JOIN log 
+			ON log.path LIKE CONCAT('%', articles.slug) 
+		GROUP BY authors.name 
+		ORDER BY views DESC;'''
+		
+		cur.execute(query)
+		rows = cur.fetchall()
+
+		print
+		print "Most popular authors:"
+		for row in rows:
+			print " ", row[0], "-", row[1], "views"
+
+		cur.close()
+		db.close()
 
 # Function to answer question: "On which days did more than 1% of requests lead to errors"
 def request_errors():
@@ -48,3 +64,4 @@ def request_errors():
 	cur = db.cursor()
 
 three_articles()
+three_authors()
